ede2b0594b868a30b86baef727a461fae318afdc
Hernan <hernan@2ulaundry.com>
2022-10-28 11:15:05 -0500

    Projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, Divider, Image, Typography, Avatar } from 'antd';
+
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+const { Title } = Typography;
+
+const socialNetworks = [
+  {
+    name: 'Facebook',
+    link: 'https://www.facebook.com/General-Consultic-101550805714481',
+    image: 'facebook.png',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/general_consultic/',
+    image: 'instagram.png',
+  },
+  {
+    name: 'Whatsapp',
+    link: `https://wa.me/+51933640145?text=Hola, deseo información por favor. 🙂`,
+    image: 'whatsapp.png',
+  },
+  {
+    name: 'Tik Tok',
+    link: 'https://www.tiktok.com/@generalconsultic',
+    image: 'tiktok.png',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ paddingTop: 20, backgroundColor: '#f1f1f1' }}>
+      <Row justify="center" style={{ }}>
+      <Title>Hola!!</Title>
+      </Row>
+      <Row justify="center" style={{ marginBottom: 20 }}>
+        <Title>Somos</Title>
+      </Row>
+      <Row justify="center" style={{ marginBottom: 20 }}>
+        <Avatar src="logo_empresa.png" size={250} />
+      </Row>
+      <Row justify="center" style={{ marginBottom: 20 }}>
+        <Title>Te invitamos a seguirnos por nuestra redes sociales</Title>
+      </Row>
+      <Row justify="center">
+        {socialNetworks.map((item) => (
+          <Col xs={{ span: 12 }} lg={{ span: 4, offset: 2 }} style={{ marginBottom: 30 }}>
+            <Row justify="center" >
+              <a href={item.link} target="_blank">
+                <Image preview={false} width={100} src={item.image} />
+              </a>
+            </Row>
+          </Col>
+        ))}
+      </Row>
+      ,
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWvFbzw-uRoGreIqKYXDdNJWNdPRg_f48",
+  authDomain: "qr-test-7327a.firebaseapp.com",
+  projectId: "qr-test-7327a",
+  storageBucket: "qr-test-7327a.appspot.com",
+  messagingSenderId: "656697589164",
+  appId: "1:656697589164:web:fa1f45ade85321c60c4e09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
